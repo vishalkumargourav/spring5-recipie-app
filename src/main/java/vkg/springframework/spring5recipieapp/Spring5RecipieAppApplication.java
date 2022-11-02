7c6b9345c323b3cdb75b0dab77b2531829bbf3b2
vishalkumargourav <vishalkumargourav@github.com>
2022-11-02 17:06:16 +0530

    Initial Commit

--- src/main/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplication.java
+++ src/main/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5RecipieAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring5RecipieAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipieAppApplication.class, args);
+	}
 
 }
--- src/test/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplicationTests.java
+++ src/test/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring5RecipieAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }

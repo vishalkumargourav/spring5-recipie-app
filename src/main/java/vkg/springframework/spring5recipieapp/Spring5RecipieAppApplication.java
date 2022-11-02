31fac0d7d68b99dbbab0fd43d9d4a359f5a6e69a
vishalkumargourav <vishalkumargourav@github.com>
2022-11-02 17:01:50 +0530

    Initial Commit

---
+++ src/main/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplication.java
@@ -0,0 +1,13 @@
+package vkg.springframework.spring5recipieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipieAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipieAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vkg/springframework/spring5recipieapp/Spring5RecipieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vkg.springframework.spring5recipieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
